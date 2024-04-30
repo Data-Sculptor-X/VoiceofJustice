dc6b49a6ef4e8972c269637751ddcf56828950a5
santhoshparthiban <santhoshparthiban2002@gmail.com>
2024-04-30 07:49:29 +0530

    changes

--- voiceofjustice/settings.py
+++ voiceofjustice/settings.py
@@ -106,9 +106,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    #    'DEFAULT_RENDERER_CLASSES': (
-    #     'rest_framework.renderers.JSONRenderer',
-    # ),
+       'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 PRIVATE_KEY_PATH = os.path.join(BASE_DIR, "secrets",'private_key.pem')
