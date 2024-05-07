a8fd7fbb472f8cb7792fc21c1441941ab691a7c2
santhoshparthiban <santhoshparthiban2002@gmail.com>
2024-05-07 10:53:55 +0530

    changes

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
-    # path('generate/', GenerateText.as_view(), name='generate_text'),
     path('generate2/', GenerateText2.as_view(), name='generate_text2'),
     path('getSection/', GetSection.as_view(), name='getSection'),
     path('getChat/', GetChat.as_view(), name='getChat'),
