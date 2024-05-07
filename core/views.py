46377e9e8f95e4a0077e4176c43a4220c31b800e
santhoshparthiban <santhoshparthiban2002@gmail.com>
2024-05-07 10:43:34 +0530

    changes

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# import google.generativeai as genai
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.contrib.auth import get_user_model
 from django.conf import settings
@@ -14,9 +13,6 @@ from rest_framework.response import Response
 from .models import *
 from accounts.models import *
 import os
-# Configure the model
-# genai.configure(api_key='AIzaSyCLfi0mijR7Xf4fvGvHRINxz5UiQYULaq8')
-# model = genai.GenerativeModel(model_name='models/gemini-pro')
 
 verification_key_path = settings.PUBLIC_KEY_PATH
 try:
@@ -33,57 +29,6 @@ def trim_sentence(sentence):
     if len(sentence) > 40:
         return sentence[:40]
     return sentence
-
-# class GenerateText(APIView):
-#     permission_classes = (IsAuthenticated,)
-
-#     def post(self, request, *args, **kwargs):
-#         prompt = request.data.get('prompt', '')
-#         response = model.generate_content(prompt)  # Assuming `model` is defined elsewhere
-
-#         if response and response.text:
-#             sectionID = request.data.get('SectionID', '')
-#             if sectionID:
-#                     section = SectionHistory.objects.get(SectionID=sectionID)
-#                     ChatHistory.objects.create(
-#                         SectionID=section,
-#                         ChatQuestion=prompt,
-#                         ChatResponse=response.text
-#                     )
-#                     return Response({'generated_text': response.text,"SectionID":sectionID})
-            
-#             else:
-
-#                 jwt_object = JWTAuthentication() 
-#                 token = jwt_object.get_raw_token(jwt_object.get_header(request))  
-#                 try:
-#                     data = jwt.decode(token, verification_key, algorithms=["RS256"])
-#                 except UnicodeDecodeError as e:
-#                     print(f"UnicodeDecodeError: {e}")
-#                     print(f"Problematic token: {token}")
-#                     print(data)
-
-#                 userTrackData = UserTrack.objects.get(id=data["tk"])
-
-
-#                 sect=generate_random_string(userTrackData.username,10)
-#                 section = SectionHistory.objects.create(
-#                                                     SectionID=sect,
-#                                                     username=userTrackData.username,
-#                                                     SectionName=trim_sentence(prompt)
-#                                                         )
-#                 if section:
-#                     ChatHistory.objects.create(
-#                         SectionID=section,
-#                         ChatQuestion=prompt,
-#                         ChatResponse=response.text
-#                     )
-#                     return Response({'generated_text': response.text,"SectionID":sect})
-#                 else:
-#                     return Response({'error': 'Section not found.'}, status=400)
-
-#         else:
-#             return Response({'error': 'Empty response from the model.'}, status=400)
 
 
 class GetSection(APIView):
